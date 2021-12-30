abb0fa9347d8d068acb137b244603f75aee8c8d3
uuhuang918 <uuhuang918@gmail.com>
2021-12-30 11:20:14 +0800

    add

---
+++ NTUB.FileManager.Site/Models/Interfaces/IDocRepository.cs
@@ -0,0 +1,18 @@
+﻿using NTUB.FileManager.Site.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTUB.FileManager.Site.Models.Interfaces
+{
+	public interface IDocRepository
+	{
+		void Create(DocEntity entity);
+		void Update(DocEntity entity);
+		void Delete(DocEntity entity);
+		IEnumerable<DocEntity> Search(string title, string description);
+		DocEntity Load(int dosId);
+	}
+}
--- ReadMe.txt
+++ ReadMe.txt
@@ -2,4 +2,4 @@
 [ ] create new asp.net project NTUB.FileManager.Site
 [ ] add Models/EFModels/ 刪除語言包,重建專案
 [ ] add Models/Entities/DocEntity class
-[ ] 
+[ ]  add Models/Interfaces/IDocRepository
