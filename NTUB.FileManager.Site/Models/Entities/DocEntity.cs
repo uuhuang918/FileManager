39c870092b366c2e8b8b0ae2d0aa587635d34d31
uuhuang918 <uuhuang918@gmail.com>
2021-12-30 11:13:21 +0800

    加入專案檔案。

---
+++ NTUB.FileManager.Site/Models/Entities/DocEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NTUB.FileManager.Site.Models.Entities
+{
+	public class DocEntity
+	{
+        public int Id { get; set; }
+
+    
+        public string Title { get; set; }
+
+     
+        public string Description { get; set; }
+
+     
+        public string FileName { get; set; }
+
+        public DateTime ModifiedTime { get; set; }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+[ ] add db & table
+[ ] create new asp.net project NTUB.FileManager.Site
+[ ] add Models/EFModels/ 刪除語言包,重建專案
+[ ] add Models/Entities/DocEntity class
+[ ] 
